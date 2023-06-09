13c70cf57d3ff09ca22831a0bd892c7f8c1791d4
Prosenjit Majumder Joy <32308244+prosenjitjoy@users.noreply.github.com>
2023-06-10 03:55:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function updateLeetCodeGist(leetcode) {
             gist_id: gist_id,
             files: {
                 [filename]: {
-                    filename: `💻 My LeetCode Stats`,
+                    filename: `💻 Daily LeetCode Stats`,
                     content: lines.join("\n")
                 }
             }
